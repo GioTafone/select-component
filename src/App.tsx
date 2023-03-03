90e0ef7e2d57ed54609a7cd0c88c58fc50b72f8a
Giovanni Tafone <giola@Giovannis-MacBook-Air.local>
2023-03-03 09:35:52 +0200

    implemented logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { options } from "./options"
-import Select from "./components/Select"
+import { useState } from "react";
+import { options } from "./options";
+import Select from "./components/Select";
 
 function App() {
+  const [value, setValue] = useState<typeof options[0] | undefined>(options[0]);
   return (
     <>
-    <h1>Hello</h1>
-    <Select options={options}/>
+      <h1>Hello</h1>
+      <Select
+        options={options}
+        value={value}
+        onChange={(option) => setValue(option)}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,16 +1,49 @@
-import { SelectProps } from "../types";
+import { useState } from "react";
+import { SelectOption, SelectProps } from "../types";
 import styles from "../styles/select.module.css";
 
 const Select = ({ value, onChange, options }: SelectProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleSelect = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+  const closeSelect = () => {
+    setIsOpen(false);
+  };
+  const clearOptions = () => {
+    onChange(undefined);
+  };
+  const selectOption = (option: SelectOption) => {
+    onChange(option)
+  }
   return (
-    <div tabIndex={0} className={styles.container}>
-      <span className={styles.value}>Value</span>
-      <button className={styles["clear-btn"]}>&times;</button>
+    <div
+      onBlur={closeSelect}
+      onClick={handleSelect}
+      tabIndex={0}
+      className={styles.container}
+    >
+      <span className={styles.value}>{value?.label}</span>
+      <button
+        className={styles["clear-btn"]}
+        onClick={(e) => {
+          e.stopPropagation()
+          clearOptions();
+        }}
+      >
+        &times;
+      </button>
       <div className={styles.divider}></div>
       <div className={styles.caret}></div>
-      <ul className={styles.options}>
-        {options.map(option => (
-          <li key={option.value} className={styles.option}>{option.label}</li>
+      <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
+        {options.map((option) => (
+          <li onClick={e => {
+            e.stopPropagation()
+            selectOption(option)
+            closeSelect()
+          }} key={option.value} className={styles.option}>
+            {option.label}
+          </li>
         ))}
       </ul>
     </div>
